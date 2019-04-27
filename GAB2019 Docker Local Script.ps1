fb210e01fe22ecbb438c703a23b83530ac77ff7d
Justin Rutledge <justin.rutledge1@gmail.com>
2019-04-27 12:57:13 -0400

    initial commit

---
+++ GAB2019 Docker Local Script.ps1
@@ -0,0 +1,31 @@
+﻿# You must have docker installed for this to work.
+
+$envVariablesFilePath = "$($env:APPDATA)\GAB2019variables.env"
+if(-not (Test-Path $envVariablesFilePath)){
+    $environmentVariables = @{
+        Email = 'youremail@domain.com'
+        Fullname = 'yourname'
+        TeamName= 'ColumbusOH'
+        CompanyName = 'idk'
+        CountryCode = 'US'
+        LabKeyCode = '3817'    
+    }
+
+    $fileContents = $environmentVariables.keys.ForEach{
+        $thisVariable = "BatchClient__$_=$($environmentVariables[$_])"
+        $thisVariable
+        
+    }
+    $fileContents | Out-File $envVariablesFilePath
+}
+
+[ValidateRange(1,20)]
+$numberOfInstances = 10
+$startPort = 8080
+$endPort = [int]($startPort + $numberOfInstances)
+$appendPort = ':80'
+
+
+($startPort..$endPort).ForEach{
+    docker run -d -p "$_$appendPort" --env-file $envVariablesFilePath --restart always globalazurebootcamp/sciencelab2019:latest
+}
